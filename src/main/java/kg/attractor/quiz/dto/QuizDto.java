81959edf3c01c0e1c02c0bd231d0b352fd2f5a26
Askar <nurbekovaskar099@gmail.com>
2025-07-31 00:48:31 +0600

    Created dto classes fot project with included validations

---
+++ src/main/java/kg/attractor/quiz/dto/QuizDto.java
@@ -0,0 +1,14 @@
+package kg.attractor.quiz.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class QuizDto {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String description;
+}
---
+++ src/main/java/kg/attractor/quiz/dto/ResultDto.java
@@ -0,0 +1,12 @@
+package kg.attractor.quiz.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+@Email
+@Builder
+public class ResultDto {
+    @NotNull
+    private int score;
+}
