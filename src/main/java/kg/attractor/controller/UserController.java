ba3543182cf2e257f7ff09faae5cb682b7d84233
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-01 01:36:49 +0600

    Added class UserController

---
+++ src/main/java/kg/attractor/controller/UserController.java
@@ -0,0 +1,12 @@
+package kg.attractor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+}
