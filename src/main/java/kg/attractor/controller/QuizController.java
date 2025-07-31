3ef82b86a1c98637b41ea584da06745427d003a3
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-01 01:34:44 +0600

    Added class QuizController

---
+++ src/main/java/kg/attractor/controller/QuizController.java
@@ -0,0 +1,22 @@
+package kg.attractor.controller;
+
+import kg.attractor.quiz.dto.QuizDto;
+import kg.attractor.quiz.service.impl.QuizServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/quizzes")
+@RequiredArgsConstructor
+public class QuizController {
+
+    private final QuizServiceImpl quizService;
+
+    @PostMapping
+    public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto) {
+        quizService.createQuiz(quizDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Quiz created");
+    }
+}
