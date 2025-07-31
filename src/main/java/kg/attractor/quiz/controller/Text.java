f83e8819b8b706cfb5636ca83c9b00e57b99af89
Kaseiinov_I <izlax6002@gmail.com>
2025-08-01 02:00:23 +0600

    dded controller to test

---
+++ src/main/java/kg/attractor/quiz/controller/Text.java
@@ -0,0 +1,23 @@
+package kg.attractor.quiz.controller;
+
+import kg.attractor.quiz.dto.QuizDto;
+import kg.attractor.quiz.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/quiz")
+@RequiredArgsConstructor
+public class Text {
+    private final QuizService quizService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<QuizDto> findById(@PathVariable Long id) {
+        QuizDto quizDto = quizService.findById(id);
+        return ResponseEntity.ok(quizDto);
+    }
+}
