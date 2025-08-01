4346ff5290bd942c4d212807b05655dcaaa3ea1e
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-01 13:54:35 +0600

    Added the class AuthController

---
+++ src/main/java/kg/attractor/quiz/controller/AuthController.java
@@ -0,0 +1,28 @@
+package kg.attractor.quiz.controller;
+
+import jakarta.validation.Valid;
+import kg.attractor.quiz.dto.UserDto;
+import kg.attractor.quiz.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserDto userDto) {
+        userService.register(userDto);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    // TODO: Добавить здесь конечные точки входа и выхода, как того требует конфигурация Spring Security.
+}
+
